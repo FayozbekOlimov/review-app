61bef5924c84179dc3a99522fb1b9d35199464a8
FayozbekOlimov <fayozbekdeveloper@gmail.com>
2023-09-08 16:35:46 +0500

    initialize backend

---
+++ server/controllers/loginController.js
@@ -0,0 +1,26 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+const handleLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password)
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+
+    const foundUser = await User.findOne({ email }).exec();
+    if (!foundUser) return res.status(401).json({ message: "Unauthorized" });
+
+    const match = await bcrypt.compare(password, foundUser.password);
+    if (!match) return res.status(401).json({ message: "Invalid credentials" });
+
+    await foundUser.save();
+
+    res.status(200).json({ message: "Login successful" });
+  } catch (err) {
+    res.status(500).json({ message: "Registration failed" });
+  }
+};
+
+module.exports = { handleLogin };
---
+++ server/controllers/registerController.js
@@ -0,0 +1,28 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+const handleNewUser = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  if (!firstName || !lastName || !email || !password)
+    return res.status(400).json({ message: "All fields are required" });
+
+  const duplicate = await User.findOne({ email }).exec();
+  if (duplicate)
+    return res.status(409).json({ message: "This email already exists" });
+
+  try {
+    const hashedPwd = await bcrypt.hash(password, 10);
+    await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPwd,
+    });
+
+    res.status(201).json({ success: "User registered successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Registration failed" });
+  }
+};
+
+module.exports = { handleNewUser };
